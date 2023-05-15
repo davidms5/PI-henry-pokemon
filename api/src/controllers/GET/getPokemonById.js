6f24ba47cec287ca3e0d1f1c3d7fb336318e6c53
david medina <rafaelsanchez97.dm@gmail.com>
2023-05-14 22:15:56 -0300

    segundo endpoint probado por ahora

--- api/src/controllers/GET/getPokemonById.js
+++ api/src/controllers/GET/getPokemonById.js
@@ -4,12 +4,20 @@ const axios = require("axios");
 const getPokemonById = async(req, res) =>{
 
     const {idPokemon} = req.params;
+    const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+    const prueba = regexExp.test(idPokemon)
     try {
         if(!idPokemon) res.status(404).send("pokemon no encontrado")
         
-        let response = await Pokemon.findOne({where:{id:idPokemon}});
+        if(prueba){
+            let response = await Pokemon.findOne({where:{id:idPokemon}});
 
-        if(!response){
+            if(!response) res.status(404).send("el pokemon no existe");//hacer test de esta linea
+
+            res.status(200).json(response);
+
+        } else {
 
             let apiResponse = await axios(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
 
@@ -29,8 +37,10 @@ const getPokemonById = async(req, res) =>{
 
             res.status(200).json(pokemon);
         }
+        
 
-        res.status(200).json(response)
+
+       
     } catch (error) {
         console.log(error.message);
         res.status(500).json({message: error.message});
