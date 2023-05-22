d9017f9aa19300477d1332a4797bed0d48b9bf5c
david medina <rafaelsanchez97.dm@gmail.com>
2023-05-22 13:37:12 -0300

    esqueleto post pokemon3

--- api/src/controllers/POST/createPokemon.js
+++ api/src/controllers/POST/createPokemon.js
@@ -2,4 +2,11 @@ const {Pokemon} = require("../../db.js");
 
 const CreatePokemon = async(req, res) =>{
 
+  const {nombre, imagen, vida, ataque, defensa, velocidad, altura, pesotipos} = req.body;
+  //puedo omitir la imagen? revisar los modelos de la base de datos
+  //revisar si el.status de abajo es el correcto
+  if(!nombre ||!vida||!ataque||!defensa||!velocidad||!altura||!peso){
+      return res.status(404).send("falta informacion para crear el pokemon");
+  }
+
 }
