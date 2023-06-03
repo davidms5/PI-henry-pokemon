48d270c9966f0f0c459ba7252fa97539b3c2ddfa
david medina <rafaelsanchez97.dm@gmail.com>
2023-06-02 23:03:55 -0300

    arreglando pequeño bug del paginado x2

--- client/src/components/MainPage/DisplayCards/Cards.jsx
+++ client/src/components/MainPage/DisplayCards/Cards.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import {Link} from "react-router-dom";
 import Card from "./Card/Card";
