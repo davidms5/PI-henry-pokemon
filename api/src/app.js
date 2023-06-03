2401bc2791dc248deee96daf93bb7349834bb96f
david medina <rafaelsanchez97.dm@gmail.com>
2023-06-02 22:41:48 -0300

    test 3 backend in cloud

--- api/src/app.js
+++ api/src/app.js
@@ -14,8 +14,14 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+
+const listaDeOrigins = ["http://localhost:3000", "https://pi-henry-pokemon-s4xa.vercel.app/"]
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  const origin = req.headers.origin;
+  if (listaDeOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
